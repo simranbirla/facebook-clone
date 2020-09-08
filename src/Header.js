3d76e886d53d838ce625887312cf044028b66511
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-09-09 00:08:25 +0530

    second commit

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://f0.pngfuel.com/png/819/241/facebook-logo-png-clip-art.png"
+          alt="logo"
+        />
+      </div>
+
+      <div className="header__middle"></div>
+
+      <div className="header__right"></div>
+    </div>
+  );
+};
+
+export default Header;
