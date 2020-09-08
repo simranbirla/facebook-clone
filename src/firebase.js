2f374d0955e204ce6ac0a458a20793227b2b1c60
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-09-08 23:55:38 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDckjdVYHMwdzg79c182S1eXzNIRhqSWoA",
+  authDomain: "facebook-clone-ffbdc.firebaseapp.com",
+  databaseURL: "https://facebook-clone-ffbdc.firebaseio.com",
+  projectId: "facebook-clone-ffbdc",
+  storageBucket: "facebook-clone-ffbdc.appspot.com",
+  messagingSenderId: "548623135403",
+  appId: "1:548623135403:web:bae388a4cbd11b1dcbbdee",
+  measurementId: "G-TFL7HNMBE6",
+};
