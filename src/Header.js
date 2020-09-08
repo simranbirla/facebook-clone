3f560c0a3eb623b6e424a099ff742b4a48c801f6
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-09-09 01:12:09 +0530

    header

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,15 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlined from "@material-ui/icons/StorefrontOutlined";
+import SupervidesUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import { Avatar, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import "./Header.css";
 const Header = () => {
   return (
@@ -8,11 +19,50 @@ const Header = () => {
           src="https://f0.pngfuel.com/png/819/241/facebook-logo-png-clip-art.png"
           alt="logo"
         />
+
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
       </div>
 
-      <div className="header__middle"></div>
+      <div className="header__center">
+        <div className="header__option header__option--active ">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlined fontSize="large" />
+        </div>
 
-      <div className="header__right"></div>
+        <div className="header__option">
+          <SupervidesUserCircleIcon fontSize="large" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4>Simran Birla</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
     </div>
   );
 };
